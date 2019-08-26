7c067f1e78bf214fabe941ab8ede93de69e9c452
Ben Baron <ben@einsteinx2.com>
2019-08-26 13:01:04 -0500

    Compacted the alpha to rgba texture code

--- src/glex/Font.cpp
+++ src/glex/Font.cpp
@@ -34,14 +34,12 @@ void Font::createTexture() {
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     
     #ifdef DREAMCAST
-    int32_t alphaSize = _font.tex_data.size();
-    int32_t rgbSize = alphaSize * 4;
-    unsigned char rgbData[rgbSize];
-    for(int i = 0; i < alphaSize; ++i) {
-        rgbData[i*4] = 0xFF;
-        rgbData[(i*4)+1] = 0xFF;
-        rgbData[(i*4)+2] = 0xFF;
-        rgbData[(i*4)+3] = _font.tex_data[i];
+    // GLdc doesn't support GL_ALPHA textures, so convert to RGBA format
+    uint32_t alphaSize = _font.tex_data.size();
+    unsigned char rgbData[alphaSize*4];
+    for(uint32_t i = 0; i < alphaSize; ++i) {
+        rgbData[i*4] = rgbData[(i*4)+1] = rgbData[(i*4)+2] = 0xFF; // Set RGB values to max
+        rgbData[(i*4)+3] = _font.tex_data[i];                      // Use existing alpha value
     }
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, _font.tex_width, _font.tex_height, 0, GL_RGBA, GL_UNSIGNED_BYTE, &rgbData[0]);
     #else
