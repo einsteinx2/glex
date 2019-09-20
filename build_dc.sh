ccd1328f548c24a65bec2fcd03f642e0851a402f
Ben Baron <ben@einsteinx2.com>
2019-09-20 03:58:46 -0500

    cmake now builds GLdc automatically

--- build_dc.sh
+++ build_dc.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-#cd /src
-#rm -rf /src/CMakeCache.txt /src/CMakeFiles /src/build/dc && cmake -DCMAKE_TOOLCHAIN_FILE=./toolchains/Dreamcast.cmake . && make -j4
 
-#rm -rf /src/build/dc && \
-#mkdir -p /src/build/dc && cd /src/build/dc && cmake -DCMAKE_TOOLCHAIN_FILE=../../toolchains/Dreamcast.cmake ../../ && \
-#cd /src/deps/dc/GLdc && make build -j4 && cd /src/build/dc && make -j4
-
-cd /src/deps/dc/GLdc && make build && cd /src && \
 mkdir -p /src/build/dc && cd /src/build/dc && cmake -DCMAKE_TOOLCHAIN_FILE=../../toolchains/Dreamcast.cmake ../../ && make -j4
