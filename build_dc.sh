fe39bc77a137f3f9c5de68df1dc520996a02565f
Ben Baron <ben@einsteinx2.com>
2020-01-09 11:40:15 -0500

    Fixed DC toolchain loading but still getting linker errors

--- build_dc.sh
+++ build_dc.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 mkdir -p /src/build/dc && cd /src/build/dc &&
-#cmake -DCMAKE_TOOLCHAIN_FILE=../../toolchains/Dreamcast.cmake ../../ &&
+cmake -DCMAKE_TOOLCHAIN_FILE=../../toolchains/Dreamcast.cmake ../../ &&
 make -j4
