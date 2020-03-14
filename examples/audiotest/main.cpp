f66c062bc1f9551ff262df494f14eb5209e6a656
Ben Baron <ben@einsteinx2.com>
2020-03-14 17:05:59 -0500

    Added basic audio test example (DC only)

---
+++ examples/audiotest/main.cpp
@@ -0,0 +1,86 @@
+#include "glex/common/log.h"
+#include "glex/Application.h"
+#include "glex/Image.h"
+#include "glex/Text.h"
+#include "glex/input/KeyboardInputHandler.h"
+
+#include <cstdio>
+#include <cstdlib>
+#include <string>
+#include <chrono>
+
+std::chrono::steady_clock::time_point currentTime = std::chrono::steady_clock::now();
+std::chrono::steady_clock::time_point lastTime = std::chrono::steady_clock::now();
+int16_t timeDiff = 0;
+uint32_t nbFrames = 0;
+float frameTime = 0.0;
+float fps = 0.0;
+
+/* program entry */
+int main(int argc, char *argv[]) {
+    DEBUG_PRINTLN("Application started!");
+    int width = 640;
+    int height = 480;
+    Application app;
+    app.createWindow("GLEX Playground", width, height);
+
+    // Basic keyboard handling
+    int lastKeyCode = -1;
+    KeyboardInputHandler keyboard;
+    keyboard.registerCallback([&app, &lastKeyCode](KeyCode code) {
+        lastKeyCode = code;
+
+        // Exit the application when escape key is pressed
+        if (code == KeyCode::Escape) {
+            DEBUG_PRINTLN("Escape pressed");
+            app.closeWindow();
+        }
+    });
+    app.addInputHandler(&keyboard);
+
+    Text infoText(FontFace::arial_16, "", FONT_COLOR_WHITE, 20, app.windowHeight() - 40, Image::Z_HUD, app.screenScale);
+    infoText.createTexture();
+
+    Text fpsCounter(FontFace::arial_16, "", FONT_COLOR_WHITE, 20, 20, Image::Z_HUD, app.screenScale);
+    fpsCounter.createTexture();
+
+    // Main loop
+    while (!app.windowShouldClose()) {
+        // Measure speed
+        nbFrames++;
+        currentTime = std::chrono::steady_clock::now();
+        timeDiff = std::chrono::duration_cast<std::chrono::milliseconds>(currentTime - lastTime).count();
+        if (timeDiff >= 1000) {
+            frameTime = float(timeDiff) / float(nbFrames);
+            fps = nbFrames;
+            nbFrames = 0;
+            lastTime = currentTime;
+        }
+
+        // Handle input
+        app.handleInput();
+
+        // Clear the buffer to draw the prepare frame
+        app.clear();
+
+        // Draw the info text
+        app.reshapeOrtho(infoText.scale);
+        static char outputString[50];
+        // NOTE: Due to an old GCC bug, we must manually cast floats to double in order to use %f without a warning 
+        sprintf(&outputString[0], "Info text goes here");
+        infoText.text = outputString;
+        infoText.draw();
+
+        // Draw the FPS counter HUD text
+        app.reshapeOrtho(fpsCounter.scale);
+        // NOTE: Due to an old GCC bug, we must manually cast floats to double in order to use %f without a warning 
+        sprintf(&outputString[0], "frame time: %.2f ms  fps: %.2f  key: %d", (double)frameTime, (double)fps, lastKeyCode);
+        fpsCounter.text = outputString;
+        fpsCounter.draw();
+
+        // Swap buffers to display the current frame
+        app.swapBuffers();
+    }
+
+    app.closeWindow();
+}
