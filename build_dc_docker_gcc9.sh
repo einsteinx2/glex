cfda0ec84b490510ea41f0b887a143ae91530e91
Ben Baron <ben@einsteinx2.com>
2020-04-15 12:43:36 -0500

    Pinned specific docker image tags in build scripts

--- build_dc_docker_gcc9.sh
+++ build_dc_docker_gcc9.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker run -it --rm -v "$PWD:/src" einsteinx2/dcdev-kos-toolchain:gcc-9 ./build_dc.sh
+# Pinning specific docker image tag to ensure building always works
+docker run -it --rm -v "$PWD:/src" einsteinx2/dcdev-kos-toolchain:gcc-9__v1.0.1 ./build_dc.sh
