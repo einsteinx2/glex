905741698a8769af01363e6452f2986a5fd64972
Ben Baron <ben@einsteinx2.com>
2020-04-28 16:12:04 -0500

    Temprarily switched to THREADS=1 for dc
    for some reason building with multiple threads is broken on macOS

--- build_dc.sh
+++ build_dc.sh
@@ -4,7 +4,8 @@
 rm -rf ./build/dc
 
 # Build using max available threads
-export THREADS=$(getconf _NPROCESSORS_ONLN) &&
+#export THREADS=$(getconf _NPROCESSORS_ONLN) &&
+export THREADS=1 &&
 mkdir -p ./build/dc && cd ./build/dc &&
 cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_TOOLCHAIN_FILE=../../cmake/toolchains/Dreamcast.cmake ../../ &&
 make VERBOSE=ON -j$THREADS
