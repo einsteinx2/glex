576a16ef25540215ea883c9aeb7b137141209ed2
Ben Baron <ben@einsteinx2.com>
2020-10-03 22:07:46 -0500

    Fixed logs to stdout not printing on Dreamcast
    Looks like the newer version of NewLib has a larger default output buffer, so adding a manual flush call takes care of it.

--- include/glex/common/log.h
+++ include/glex/common/log.h
@@ -4,12 +4,12 @@
 
 #if DEBUG
 #include <cstdio>
-#define DEBUG_PRINT(...) printf(__VA_ARGS__)
-#define DEBUG_PRINTLN(...) do { printf(__VA_ARGS__); puts(""); } while (0)
+#define DEBUG_PRINT(...) do { printf(__VA_ARGS__); fflush(stdout); } while (0)
+#define DEBUG_PRINTLN(...) do { printf(__VA_ARGS__); puts(""); fflush(stdout); } while (0)
 #else
 #define DEBUG_PRINT(...)
 #define DEBUG_PRINTLN(...)
 #endif
 
-#define ERROR_PRINT(...) fprintf(stderr, __VA_ARGS__)
-#define ERROR_PRINTLN(...) do { fprintf(stderr, __VA_ARGS__); puts(""); } while (0)
+#define ERROR_PRINT(...) do { fprintf(stderr, __VA_ARGS__); fflush(stderr); } while (0)
+#define ERROR_PRINTLN(...) do { fprintf(stderr, __VA_ARGS__); puts(""); fflush(stderr); } while (0)
