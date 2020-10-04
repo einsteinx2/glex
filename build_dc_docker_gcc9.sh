edf187ba3ac0fac36a04dd404fae1aae2cec999e
Ben Baron <ben@einsteinx2.com>
2020-10-04 17:21:23 -0500

    Updated dcdev-kos-toolchain docker image tag
    Rebuilt the images using latest KOS toolchain and confirmed they still compile the project without issue.

--- build_dc_docker_gcc9.sh
+++ build_dc_docker_gcc9.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Pinning specific docker image tag to ensure building always works
-docker run -it --rm -v "$PWD:/src" einsteinx2/dcdev-kos-toolchain:gcc-9__v1.0.1 ./build_dc.sh
+docker run -it --rm -v "$PWD:/src" einsteinx2/dcdev-kos-toolchain:gcc-9__v1.0.2 ./build_dc.sh
